f7e56fb0a4fed857f2fa410b0feaefe7c8f6e14a
Tyler <Tylerrayrice2@gmail.com>
2022-05-24 15:23:50 -0600

    Commit Items hard coded on Page

--- app/Models/Snack.js
+++ app/Models/Snack.js
@@ -1,12 +1,15 @@
+import { generateId } from "../Utils/generateId"
 
 
 
 export class Snack {
 
     constructor(name, price) {
-
+        this.id = generateId()
         this.name = name
         this.price = price
     }
+
+
 
 }
--- app/Services/SnackService.js
+++ app/Services/SnackService.js
@@ -1,12 +1,18 @@
 import { SnackController } from "../Controllers/SnackController.js";
 
 
+let money = 0
+
 class SnackService {
 
     serviceClick() {
-        console.log("Hi from your service controller");
+        console.log("Hi from your service controller", money);
+
+        money += 0.25
 
     }
+
+    buy
 
 }
 
