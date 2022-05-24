aa2f44096f4a7a345c269b013bf66e9c23ad64db
Tyler <Tylerrayrice2@gmail.com>
2022-05-24 13:57:07 -0600

    Commited Created Folders for Snacks

--- app/AppState.js
+++ app/AppState.js
@@ -2,8 +2,8 @@ import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-  /** @type {import('./Models/Value').Value[]} */
-  values = []
+  // /** @type {import('./Models/Value').Value[]} */
+  // values = []
 }
 
 export const ProxyState = new Proxy(new AppState(), {
--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 import { ValuesController } from "./Controllers/ValuesController.js";
 
 class App {
-  valuesController = new ValuesController();
+  // valuesController = new ValuesController();
+
+
+
 }
 
 window["app"] = new App();
